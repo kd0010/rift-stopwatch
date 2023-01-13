5de2101847d6d98ce2c45536c554a9902ae827bd
David Kanders <kanders.davids@gmail.com>
2023-01-13 07:31:52 +0200

    move `index.ts`

---
+++ index.ts
@@ -0,0 +1,7 @@
+export { createClock } from './src/createClock'
+export { getTimePassed } from './src/getTimePassed'
+export { pauseClock } from './src/pauseClock'
+export { resumeClock } from './src/resumeClock'
+
+// types
+export * from './src/types/types'
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { createClock } from './createClock'
-export { getTimePassed } from './getTimePassed'
-export { pauseClock } from './pauseClock'
-export { resumeClock } from './resumeClock'
-
-// types
-export * from './types/types'
